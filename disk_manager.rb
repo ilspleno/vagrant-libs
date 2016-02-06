5598e072a6fe0057440aacef4e111f6b6711a06b
Brian Woodruff <brian@thewoodruffs.org>
2016-02-06 15:24:44 -0500

    Refactored disk_manager to take a hash describing disks to be created.
    Wonder if I should make it YAML?

--- disk_manager.rb
+++ disk_manager.rb
@@ -1,53 +1,76 @@
 # vi: set ft=ruby :
 #
 # Called by Vagrantfile, not a standalone script
+#
+require 'pp'
 
-def create_asm_disks(config, vb, project_name, do_attach)
+disk_layout = { :project => 'oradb12-single',
+                :shareable => false,
+                :create => true,
+                :groups => [
+                            { :prefix => "ocr",
+                              :num    => 3,
+                              :size   => 3     },
+                            { :prefix => "disk",
+                              :num    => 5,
+                              :size   => 3
+                            }
+                           ]
+              }
+
+
+def create_asm_disks(config, vb, options)
+
+	# Options:
+	# 		prefix => name to prepend to disk volume
+	# 		project => name to assign to all disks
+	# 		create => flag to create or just attach 
+	# 		num => number of disks to create
+	# 		size => size of disks in GB
+	# 		shareable => true if disks will be shared (RAC). If not they will be thin provisioned, otherwise have to be thick
+
+	#pp options
+	
+	# Load default values
+	project_name 	= options[:project] || "default_project"
+	create		= options[:create] || true
+	shareable	= options[:shareable] || false
 
 	diskport = 1
 	diskpath=`VBoxManage list systemproperties | grep "Default machine folder" | awk ' { print $4; } '`.chomp
 
-	# Create OCRVOTE disks
-	(1..OCR_DISKS).each do |n|
-		if ENV['OS'].match /windows/i 
-			disk = diskpath + "\\ocrdisk_#{project_name}_#{n}.vdi"
-		else
-			disk = diskpath + "/ocrdisk_#{project_name}_#{n}"
-		end
+	options[:groups].each do |group|
 
-	       	
-		if !do_attach or File.exist? (disk.gsub /\\\\/, '\\')
-			# Just attach
+		disk_prefix = group[:prefix] || 'default-prefix'
+                num_disks   = group[:num]    || 1
+                disk_size   = group[:size]   || 1
+
+		(1..num_disks).each do |n|
+			
+			# Change path based on Windows vs. Linux
+			if ENV['OS'].match /windows/i
+				disk = diskpath + "\\#{project_name}_#{disk_prefix}_#{n}.vdi"
+			else
+				disk = diskpath + "/#{project_name}_#{disk_prefix}_#{n}.vdi"
+			end
+
+			# If we were asked to create disk, and it's not already there...
+			if create && !File.exist?(disk.gsub /\\\\/, '\\')
+				if shareable
+					variant='Fixed'
+				else
+					variant='Standard'
+				end
+
+				# Create the disks
+				vb.customize ['createhd', '--filename', disk, '--size', disk_size * 1024, '--variant', variant]
+				vb.customize ['modifyhd', disk, '--type', 'shareable'] if shareable
+			end
+
+			# Either way, attach the disk
 			vb.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--device', 0, '--port', diskport, '--type', 'hdd', '--medium', disk]
 			diskport += 1
-
-		else
-			# Create and attach
-			vb.customize ['createhd', '--filename', disk, '--size', OCR_DISK_SIZE * 1024, '--variant', 'Fixed']
-			vb.customize ['modifyhd', disk, '--type', 'shareable']
-			vb.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--device', 0, '--port', diskport, '--type', 'hdd', '--medium', disk]
-			diskport += 1
 		end
+
 	end
-
-	# Create ASM disks
-	(1..ASM_DISKS).each do |n|
-		if ENV['OS'].match /windows/i 
-			disk = diskpath + "\\asmdisk_#{project_name}_#{n}.vdi"
-		else
-			disk = diskpath + "/asmdisk_#{project_name}_#{n}"
-		end
-
-		if !do_attach or File.exist? (disk.gsub /\\\\/, '\\')
-			vb.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--device', 0, '--port', diskport, '--type', 'hdd', '--medium', disk]
-			diskport += 1
-		else
-			vb.customize ['createhd', '--filename', disk, '--size', ASM_DISK_SIZE * 1024, '--variant', 'Fixed']
-			vb.customize ['modifyhd', disk, '--type', 'shareable']
-			vb.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--device', 0, '--port', diskport, '--type', 'hdd', '--medium', disk]
-			diskport += 1
-		end
-	end
-
-
 end
